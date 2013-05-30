c29aab57d7ba65fb9bf05f907ccee46af6895311
draegtun <draegtun@googlemail.com>
2013-05-30 18:58:05 +0100

    removed virgin build shell scripts

--- build/diff.sh
+++
@@ -1,8 +0,0 @@
-r3="/Users/barry/Local/Rebol/github-r3/make/r3 -qs"
-$r3 build-test-html-files.r3
-$r3 build-lang-rebol-js.r3
-diff test-rebol-sunburst.html ../test-rebol-sunburst.html 
-diff test-rebol-Stackoverflow.html ../test-rebol-Stackoverflow.html 
-diff test-rebol.html ../test-rebol.html 
-diff not-working.html ../not-working.html
-diff lang-rebol.js ../lang-rebol.js
